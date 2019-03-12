eb4ff291b448659d1df64e6287c7c59161af4009
martinrocket <martin.ruble@gmx.com>
2019-03-12 09:50:01 -0600

    add test for all OS

--- BJIv1/started_test.go
+++ BJIv1/started_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "testing"
-    "runtime"
+	"runtime"
+	"testing"
 )
 
 func TestStart(t *testing.T) {
@@ -14,10 +14,12 @@ func TestStart(t *testing.T) {
 	}
 }
 
-func TestOS(t *testing.T){
-    want :="linux"
-    got := clearScreen(runtime.GOOS)
-    if want != got {
-		t.Errorf("wanted %v but got %v", want, got)
+func TestOS(t *testing.T) {
+	want1 := "linux"
+	want2 := "darwin"
+	want3 := "windows"
+	got := clearScreen(runtime.GOOS)
+	if want1 != got || want2 != got || want3 != got {
+		t.Errorf("wanted %v, %v, or %v, but got %v", want1, want2, want3, got)
 	}
 }
